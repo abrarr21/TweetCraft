6683cd6cfd6a16d4051b39235c96ba6c8d26139c
Abrar <abrarrr2110@gmail.com>
2025-08-07 18:02:47 +0530

    upstash redis setup and migrations to prod db

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,3 +1,13 @@
-import Redis from "ioredis";
-
-export const redis = new Redis(process.env.REDIS_URL!);
+// redis.ts
+export const redis = (async () => {
+    if (process.env.NODE_ENV === "production") {
+        const { Redis } = await import("@upstash/redis");
+        return new Redis({
+            url: process.env.UPSTASH_REDIS_REST_URL!,
+            token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+        });
+    } else {
+        const Redis = (await import("ioredis")).default;
+        return new Redis(process.env.REDIS_URL!);
+    }
+})();
